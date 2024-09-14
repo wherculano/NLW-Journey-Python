a2a452fc9589b3447d95c745ffe582e1e28cf47c
Wagner Herculano <wagherculano@hotmail.com>
2024-09-13 22:31:26 -0300

    feat: Added send email

--- src/controllers/trip_creator.py
+++ src/controllers/trip_creator.py
@@ -1,6 +1,8 @@
 from typing import Dict
 from uuid import uuid4
 
+from src.drivers.email_sender import send_email
+
 
 class TripCreator:
     def __init__(self, trip_repository, emails_repository) -> None:
@@ -14,11 +16,16 @@ class TripCreator:
             trip_id = str(uuid4())
             trip_infos = {**body, "id": trip_id}
 
+            send_email(
+                [body["owner_email"]],
+                f"http://localhost:3000/trips/{trip_id}/confirm"
+            )
+
             self.__trip_repository.create_trip(trip_infos)
 
             if emails:
                 for email in emails:
-                    self.__emails_repository.registry_email(
+                    self.__emails_repository.register_email(
                         {"email": email, "trip_id": trip_id, "id": str(uuid4())}
                     )
 
