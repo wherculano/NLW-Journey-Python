af805ce78f03228ab8e6fdeca2f5a8b04bb8922e
Wagner Herculano <wagherculano@hotmail.com>
2024-09-13 22:27:43 -0300

    feat: Added participant creator

---
+++ src/controllers/participant_creator.py
@@ -0,0 +1,32 @@
+from uuid import uuid4
+from typing import Dict
+
+
+class ParticipantCreator:
+    def __init__(self, participants_repository, emails_repository) -> None:
+        self.__participants_repository = participants_repository
+        self.__emails_repository = emails_repository
+
+    def create(self, body, trip_id) -> Dict:
+        try:
+            participant_id = str(uuid4())
+            email_id = str(uuid4())
+
+            emails_infos = {"email": body["email"], "id": email_id, "trip_id": trip_id}
+
+            participant_infos = {
+                "id": participant_id,
+                "trip_id": trip_id,
+                "emails_to_invite_id": email_id,
+                "name": body["name"],
+            }
+
+            self.__emails_repository.register_email(emails_infos)
+            self.__participants_repository.register_participant(participant_infos)
+
+            return {"body": {"participant_id": participant_id}, "status_code": 201}
+        except Exception as exception:
+            return {
+                "body": {"error": "Bad Request", "message": str(exception)},
+                "status_code": 400,
+            }
